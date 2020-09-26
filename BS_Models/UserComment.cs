3dddb30b2af504662c845f97b2108144c8ca9390
Habibur Rahman <hrhabib.jewel6767@gmail.com>
2020-09-26 16:40:50 +0600

    initial

---
+++ BS_Models/UserComment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BS_Models
+{
+    public class UserComment
+    {
+        public DateTime ActionDate { get; set; }
+        public string CommentsCode { get; set; }
+        public string PostCode { get; set; }
+
+        public string UserCode { get; set; }
+        public string ActionType { get; set; }
+        public string Comments { get; set; }
+        public UserVote VotetypeList;
+    }
+}
---
+++ BS_Models/UserPost.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BS_Models
+{
+    public class UserPost
+    {
+        public DateTime ActionDate { get; set; }
+        public string PostCode { get; set; }
+
+        public string UserCode { get; set; }
+        public string ActionType { get; set; }
+        public string Post { get; set; }
+
+        public List<UserComment> CommentsList;
+    }
+}
